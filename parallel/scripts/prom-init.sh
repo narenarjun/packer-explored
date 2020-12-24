014d7b545155b50eece807dc8569560b1aec9d7d
Narendran <narendev0610@gmail.com>
2020-12-24 14:25:03 +0530

    parallel building for aws & lxd  with single packer.json file explored and done right

---
+++ parallel/scripts/prom-init.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+SUDO=''
+
+if (($EUID != 0)); then
+    SUDO="sudo"
+fi    
+
+
+$SUDO useradd --no-create-home --shell /bin/false prometheus
+$SUDO mkdir /etc/prometheus
+$SUDO mkdir /var/lib/prometheus
+$SUDO chown prometheus:prometheus /var/lib/prometheus
+(cd /tmp/; curl -sL https://github.com/prometheus/prometheus/releases/download/v2.20.0/prometheus-2.20.0.linux-amd64.tar.gz -O)
+(cd /tmp/; tar -xvf prometheus-2.20.0.linux-amd64.tar.gz)
+$SUDO mv /tmp/prometheus-2.20.0.linux-amd64/console* /etc/prometheus
+$SUDO mv /tmp/prometheus-2.20.0.linux-amd64/prometheus.yml /etc/prometheus
+$SUDO chown -R prometheus:prometheus /etc/prometheus
+$SUDO mv /tmp/prometheus-2.20.0.linux-amd64/prometheus /usr/local/bin/
+$SUDO mv /tmp/prometheus-2.20.0.linux-amd64/promtool /usr/local/bin/
+$SUDO chown prometheus:prometheus /usr/local/bin/prometheus
+$SUDO chown prometheus:prometheus /usr/local/bin/promtool
---
+++ parallel/scripts/prom-post.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+SUDO=''
+
+if (($EUID != 0)); then
+    SUDO="sudo"
+fi 
+
+$SUDO mv /tmp/prometheus.service /etc/systemd/system/
+$SUDO systemctl start prometheus
+$SUDO systemctl enable prometheus
